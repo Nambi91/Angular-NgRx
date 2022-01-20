202117f00e0a6ef2a571073ba4981e60fb3ca5f1
Nambi91 <nambi.rakotoarimanana@gmail.com>
2022-01-20 15:22:33 +0300

    Angular application setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,9 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, NavbarComponent],
+  imports: [BrowserModule, StoreModule.forRoot({}), AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { CustomerAddComponent } from './customer-add/customer-add.component';
 import { CustomerEditComponent } from './customer-edit/customer-edit.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerComponent } from './customer/customer.component';
 
-
-
+const customerRoutes: Routes = [{ path: '', component: CustomerComponent }];
 @NgModule({
   declarations: [
     CustomerAddComponent,
     CustomerEditComponent,
     CustomerListComponent,
-    CustomerComponent
+    CustomerComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(customerRoutes)],
 })
-export class CustomersModule { }
+export class CustomersModule {}
